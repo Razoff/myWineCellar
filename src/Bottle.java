7d7a6eb9dc4ae3a4d25547645a356c43be54306b
Arnaud Garin <arnaud.garin@epfl.ch>
2019-07-14 17:09:01 +0200

    Bottle squeletton

---
+++ src/Bottle.java
@@ -0,0 +1,48 @@
+
+public class Bottle {
+    private int year;
+    private String color;
+    private String name;
+    private String appelation;
+    private double price;
+    private int quantity;
+    private String comment;
+
+    public Bottle(int year, String color, String name, String appelation, double price, int quantity, String comment){
+        this.year = year;
+        this.color = color;
+        this.name = name;
+        this.appelation = appelation;
+        this.price = price;
+        this.quantity = quantity;
+        this.comment = comment;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAppelation() {
+        return appelation;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+}
