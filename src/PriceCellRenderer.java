58533f1d3acfd0dfb0d24b9bcb401849ca0c298c
Arnaud Garin <arnaud_lhc@hotmail.com>
2019-08-22 17:54:26 +0200

    Special price cell renderer

---
+++ src/PriceCellRenderer.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
+import java.text.DecimalFormat;
+
+public class PriceCellRenderer extends DefaultTableCellRenderer {
+    private static final DecimalFormat formatter = new DecimalFormat( "#.00" );
+
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+
+        // Format the cell value as required
+        value = formatter.format((Number)value);
+        super.setHorizontalAlignment(JLabel.LEFT);
+
+        // Pass to parent class
+        return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column );
+    }
+}
